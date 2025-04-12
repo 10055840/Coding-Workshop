930293d4fe7a329a5b83e11c2e0babd0f640ed52
10055840 <10055840@network.rca.ac.uk>
2025-04-13 00:07:53 +0100

    Add files via upload

---
+++ Coding Workshop/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(800, 800);
+  }
+  
+  function draw() {
+    background(100,0,180);
+    
+    
+    
+  circle(width/2,height/2,40)
+  //createP("this is a circle")  
+  }
+  
